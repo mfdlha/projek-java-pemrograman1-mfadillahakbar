e17fc45583028fab3abd4e2b398749fceeb01230
mfdlha <mfadillahakbar877@gmail.com>
2026-04-05 20:41:16 +0700

    feat:calculating process presensi,tugas,uts,dan uas

---
+++ p4/src/latihan/HitungNilaiAkhirMatkul.java
@@ -0,0 +1,9 @@
+package latihan;
+
+import java.util.Scanner;
+
+public class HitungNilaiAkhirMatkul {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+    }
+}
